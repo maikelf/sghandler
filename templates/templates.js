d0851db6e6e0be34d8e1cc0accafb1884e6b8f84
maikelf <maikelf@gmail.com>
2022-03-17 16:48:04 -0500

    update template

--- templates/templates.js
+++ templates/templates.js
@@ -1,13 +1,14 @@
-"use strict";
+import { createRequire } from "module"
+const require = createRequire(import.meta.url)
 
-const box_dataset_template = require("./box_dataset_template.json");
-const correlation_dataset_template = require("./correlation_dataset_template.json");
-const histogram_and_pareto_dataset_template = require("./histogram_and_pareto_dataset_template.json");
-const parallel_coordinates_dataset_template = require("./parallel_coordinates_dataset_template.json");
-const ranking_dataset_template = require("./ranking_dataset_template.json");
-const scatter_dataset_template = require("./scatter_dataset_template.json");
-const summary_dataset_template = require("./summary_dataset_template.json");
-const trend_dataset_template = require("./trend_dataset_template.json");
+const box_dataset_template =  require("./box_dataset_template.json");
+const correlation_dataset_template =  require("./correlation_dataset_template.json");
+const histogram_and_pareto_dataset_template =  require("./histogram_and_pareto_dataset_template.json");
+const parallel_coordinates_dataset_template =  require("./parallel_coordinates_dataset_template.json");
+const ranking_dataset_template =  require("./ranking_dataset_template.json");
+const scatter_dataset_template =  require("./scatter_dataset_template.json");
+const summary_dataset_template =  require("./summary_dataset_template.json");
+const trend_dataset_template =  require("./trend_dataset_template.json");
 
 const templates = {
     box_dataset_template,
